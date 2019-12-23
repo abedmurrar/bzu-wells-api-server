0baab95c9ad01bdc4ff104174263a2418be002bd
Abed Murrar <abedmurrar15@gmail.com>
2019-12-23 10:19:56 +0200

    add cors

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const logger = require('morgan');
 const session = require('express-session');
 const helmet = require('helmet');
 const createError = require('http-errors');
-
-/* Routes */
-const {wellsRouter, usersRouter} = require('./routes');
+const cors = require('cors');
 
 /* Session & Database configuration */
-const {Model} = require('objection');
+const { Model } = require('objection');
 const KnexSessionStore = require('connect-session-knex')(session);
+const { wellsRouter, usersRouter } = require('./api');
+/* Routes */
 const knex = require('./db/config');
+
 Model.knex(knex);
 
 /* Start Express App */
@@ -19,6 +20,13 @@ const app = express();
 app.set('trust proxy', 1); // trust first proxy
 
 /* Middleware */
+app.use(
+    cors({
+        origin: true,
+        credentials: true
+    })
+);
+
 app.use(
     helmet({
         xssFilter: true,
@@ -33,33 +41,39 @@ app.use(
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(
     session({
-        store: new KnexSessionStore({knex, tablename: 'sessions'}),
+        store: new KnexSessionStore({ knex, tablename: 'sessions' }),
         resave: true,
         saveUninitialized: true,
-        secret: '@@W@@', //TODO: Change it for production
-        name: ']cUpQwIOA~gLUedE/`u6DHzI"{j$f;',
+        secret: '@@W@@', // TODO: Change it for production
+        name: 'k',
         cookie: {
             signed: true,
-            maxAge: 30000 // 30 seconds for testing
+            secure: false,
+            maxAge:
+                1000 /* 1000ms */ *
+                60 /* seconds */ *
+                60 /* minutes */ *
+                24 /* hours */ *
+                365 /* days */
         }
     })
 );
 /* Set up Routers */
-app.use('/users', usersRouter);
-app.use('/wells', wellsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/wells', wellsRouter);
 
 /* Catch 404 and forward to error handler */
 app.use((req, res, next) => next(createError(404)));
 
 /* Error Handler */
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
     // render the error page
     res.status(err.status || 500);
     res.json(err);
